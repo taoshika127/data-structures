dabddbc5ae0461eb994e8e02eae96ef244e39b07
Sijia Tao <taoshika127@gmail.com>
2022-06-05 16:09:06 -0700

    Complete sprint one

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,40 @@
 class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  constructor() {
+  constructor(storage) {
+    storage = storage || {};
+    this.storage = storage;
   }
 
+  push (value) {
+    var storage = this.storage;
+    var previousElement = storage[0];
+    for (var key in storage) {
+      var newKey = Number(key) + 1;
+      [storage[newKey], previousElement] = [previousElement, storage[newKey]];
+    }
+    storage[0] = value;
+  }
+
+  pop () {
+    var storage = this.storage;
+    var popElement = storage[0];
+    for (var key in storage) {
+      if (Number(key) > 0) {
+        var newKey = Number(key) - 1;
+        storage[newKey] = storage[key];
+      }
+    }
+    delete storage[key];
+    return popElement;
+  }
+
+  size () {
+    var storage = this.storage;
+    var size = 0;
+    for (var key in storage) {
+      size++;
+    }
+    return size;
+  }
 }
