25203e880dd550b53c0fa5fd0ad8e539e885bfc8
Sijia Tao <taoshika127@gmail.com>
2022-06-06 19:34:05 -0700

    (init)

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,46 +1,6 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  var obj = {};
-  storage = {};
-  extend(obj, queueMethods);
-  return obj;
 };
 
-var storage = {};
-
-var extend = function (a, b) {
-  for (var key in b) {
-    a[key] = b[key];
-  }
-};
-
-var queueMethods = {
-
-  enqueue: function (value) {
-    var size = queueMethods.size();
-    storage[size] = value;
-  },
-
-  dequeue: function () {
-    var dequeueElement = storage[0];
-    for (var key in storage) {
-      if (Number(key) > 0) {
-        var newKey = Number(key) - 1;
-        storage[newKey] = storage[key];
-      }
-    }
-    delete storage[key];
-    return dequeueElement;
-  },
-
-  size: function () {
-    var size = 0;
-    for (var key in storage) {
-      size++;
-    }
-    return size;
-  }
-};
-
-
+var queueMethods = {};
--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,28 +7,12 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
-    var size = someInstance.size();
-    storage[size] = value;
   };
 
   someInstance.dequeue = function() {
-    var dequeueElement = storage[0];
-    for (var key in storage) {
-      if (Number(key) > 0) {
-        var newKey = Number(key) - 1;
-        storage[newKey] = storage[key];
-      }
-    }
-    delete storage[key];
-    return dequeueElement;
   };
 
   someInstance.size = function() {
-    var size = 0;
-    for (var key in storage) {
-      size++;
-    }
-    return size;
   };
 
   return someInstance;
