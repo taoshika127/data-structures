3481204ac85e480ab4defae81c645c7188a64c69
Sijia Tao <taoshika127@gmail.com>
2022-06-11 11:33:02 -0700

    Complete Linked List

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,40 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = new Node(value);
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
   };
 
   list.removeHead = function() {
+    if (this.head === null) {
+      return undefined;
+    } else {
+      var first = this.head.value;
+      this.head = this.head.next;
+    }
+    return first;
   };
 
   list.contains = function(target) {
+    if (this.head === null) {
+      return false;
+    } else {
+      var current = this.head;
+      while (current !== null) {
+        if (current.value === target) {
+          return true;
+        } else {
+          current = current.next;
+        }
+      }
+      return false;
+    }
   };
 
   return list;
@@ -23,6 +51,9 @@ var Node = function(value) {
 
   return node;
 };
+
+// Create a new node newNode
+var newNode = new Node(value);
 
 /*
  * Complexity: What is the time complexity of the above functions?
