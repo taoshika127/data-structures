a10f191c6d9134fe1722e0dc3971f4e2e21b6184
Sijia Tao <taoshika127@gmail.com>
2022-06-14 20:01:33 -0700

    Complete Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,31 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (!this._storage.hasOwnProperty(item)) {
+    this._storage[item] = item;
+  }
+
 };
 
 setPrototype.contains = function(item) {
+  return this._storage.hasOwnProperty(item);
 };
 
 setPrototype.remove = function(item) {
+  if (this.contains(item)) {
+    delete this._storage[item];
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+.add: O(1);
+.contains: O(1);
+.remove: O(1);
  */
